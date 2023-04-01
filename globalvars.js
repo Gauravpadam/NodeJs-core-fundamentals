9b7c4efc037b29e9a0efb0f970d7490d7591e0d5
Gaurav <1032201077@tcetmumbai.in>
2023-04-01 05:46:16 +0530

    Global vars done

---
+++ globalvars.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDOW!
+
+// __dirname - gives directory name
+// __filename - gives file name
+// require - function to use modules (commonJS)
+// module - info about current module (file)
+// process - info about the env where the program is being executed
+
+
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('Hello world');
+},1000)
