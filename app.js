8600d76bc04bfbe958bdba12de5fdf4cf010c15b
Gaurav <1032201077@tcetmumbai.in>
2023-04-02 08:47:41 +0530

    Included alternate-flavor amendments

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const names = require('./names')
 const sayHi = require('./namesprinter')
-
+const data = require('./alternate-flavor')
+console.log(data)
 
 sayHi(names.name2)
 sayHi(names.name3)
+sayHi(data.singlePerson.name)
+
